073e770442f203077a83d453d0de5c4ac0098a3b
Dobrodete1 <kozho31203@gmail.com>
2022-07-26 12:51:03 +0600

    feat: add Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import logo from '../../img/logo/LOgoFood-planet.svg'
+import styles from './Header.module.css';
+import phone from '../../img/logo/phone.svg'
+import korzina from '../../img/logo/korzina.svg'
+import arrow from '../../img/logo/arrow.svg'
 
 const Header = () => {
   return (
-    <div>
-        <ul>
-            <li>Главная</li>
-            <li>Меню</li>
-            <li>Доставка</li>
-            <li>Контакт</li>
-            <li>+996504405988</li>
-            <li>1</li>
-        </ul>
-    </div>
+    <>
+    <header className={styles.header}>
+      <img src={logo} alt="" />
+      <ul>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">Меню<img src={arrow} alt="" /></a></li>
+        <li><a href="#">Доставка</a></li>
+        <li><a href="#">Контакты</a></li>
+        <li><a href="#"><img src={phone} alt="" />+996500405988</a></li>
+        <li><a href="#"><img src={korzina} alt="" /> 1</a></li>
+      </ul>
+    </header>
+    </>
   )
 }
 
