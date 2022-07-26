1f746e17e5b9be58e982a4d2ce38dd89e490a946
Dobrodete1 <kozho31203@gmail.com>
2022-07-26 12:15:36 +0600

    feat:  add components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <ul>
+            <li>Главная</li>
+            <li>Меню</li>
+            <li>Доставка</li>
+            <li>Контакт</li>
+            <li>+996504405988</li>
+            <li>1</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
