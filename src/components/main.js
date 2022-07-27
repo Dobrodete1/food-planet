948f1ab02aba1de983620a1645bbe86aedf82a09
Dobrodete1 <kozho31203@gmail.com>
2022-07-27 19:10:02 +0600

    feat: app object priceBurger

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,34 @@
 export const priceBurger = [
     {
-        img:'C:\Users\Notnik_kg\Desktop\Food Planet\food-planet\src\img\product\burger\first.png';
-        name:'Чизбургер';
-        compound:''
+        img:'C:\Users\Notnik_kg\Desktop\Food Planet\food-planet\src\img\product\burger\first.png',
+        name:'Чизбургер',
+        compound:['Булка','котлета','сыр','соленый огурец','лук','помидор,',
+            'салат айсберг,','соус чесночный,','соус гриль,',
+            'кетчуп,','майонез'],
+        price:'200 сом',
+    },
+    {
+        img:'C:\Users\Notnik_kg\Desktop\Food Planet\food-planet\src\img\product\burger\second.png',
+        name:'Чизбургер',
+        compound:['Булка','котлета','сыр','соленый огурец','лук','помидор,',
+            'салат айсберг,','соус чесночный,','соус гриль,',
+            'кетчуп,','майонез'],
+        price:'200 сом',
+    },
+    {
+        img:'C:\Users\Notnik_kg\Desktop\Food Planet\food-planet\src\img\product\burger\theerth-burger.png',
+        name:'Чизбургер',
+        compound:['Булка','котлета','сыр','соленый огурец','лук','помидор,',
+            'салат айсберг,','соус чесночный,','соус гриль,',
+            'кетчуп,','майонез'],
+        price:'200 сом',
+    },
+    {
+        img:'C:\Users\Notnik_kg\Desktop\Food Planet\food-planet\src\img\product\burger\fouth-burger.png',
+        name:'Чизбургер',
+        compound:['Булка','котлета','сыр','соленый огурец','лук','помидор,',
+            'салат айсберг,','соус чесночный,','соус гриль,',
+            'кетчуп,','майонез'],
+        price:'200 сом',
     }
 ]
