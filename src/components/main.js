b08b4871bc887edfcc231d56c97f77999f09e95a
Dobrodete1 <kozho31203@gmail.com>
2022-07-27 10:02:55 +0600

    feat: add price

---
+++ src/components/main.js
@@ -0,0 +1,7 @@
+export const priceBurger = [
+    {
+        img:'C:\Users\Notnik_kg\Desktop\Food Planet\food-planet\src\img\product\burger\first.png';
+        name:'Чизбургер';
+        compound:''
+    }
+]
