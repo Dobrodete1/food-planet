30077f230edfaeb571e724f7f7e0d2c974bbdc0d
Dobrodete1 <kozho31203@gmail.com>
2022-07-28 13:32:44 +0600

    feat: add questions, reviews blogs in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 import Price from './components/NewPrice/Price';
 import Menu from './components/Menu/Menu';
+import Questions from './components/Questions/Questions';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (<>
@@ -12,6 +14,8 @@ function App() {
       <Content/>
       <Price/>
       <Menu/>
+      <Questions/>
+      <Reviews/>
     </div>
     </>
   );
