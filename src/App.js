5ff386501824876ffb43470074065cb3adf996c3
Dobrodete1 <kozho31203@gmail.com>
2022-07-31 21:27:56 +0600

    feat: app react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
@@ -11,13 +12,20 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (<>
   <Header/>
-    <div className="App">
+  <Routes className="App">
+    <Route path="/" element={<Content />} />
+    <Route path="/price" element={<Price />} />
+    <Route path="/menu" element={<Menu />} />
+    <Route path="/questions" element={<Questions/>} />
+    <Route path="/reviews" element={<Reviews />} />
+  </Routes>
+    {/* <div >
       <Content/>
       <Price/>
       <Menu/>
       <Questions/>
       <Reviews/>
-    </div>
+    </div> */}
     <Footer/>
     </>
   );
