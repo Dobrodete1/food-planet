0b62a66ad9d3eaf0c7e3c6a7459c8760b9eed9f6
Dobrodete1 <kozho31203@gmail.com>
2022-08-19 15:08:05 +0600

    feat: add Contacts and Delivery pages

---
+++ src/page/Contacts/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <div style={{'textAlign': 'center'}}>
+            <h1>Контактный номер: +996500400225</h1>
+        </div>
+    );
+};
+
+export default Contacts;
--- src/page/Delivery/Delivery.jsx
+++ src/page/Delivery/Delivery.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from '../Delivery/Delivery.module.css';
 
 const Delivery = () => {
     return (
-        <div>
-            <h1>Эта страничка с Доставкой</h1>
+        <div className={styles.conteiners}>
+            <h1>Эта страничка с Доставкой!!!</h1>
         </div>
     );
 };
 
-export {Delivery};
+export default Delivery;
