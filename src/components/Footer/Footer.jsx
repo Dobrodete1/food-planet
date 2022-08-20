7ccd14d5204803b3056e7d86ab7185f267b54fd5
Dobrodete1 <kozho31203@gmail.com>
2022-08-20 14:15:52 +0600

    feat: add link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,16 +4,18 @@ import phone from '../../img/logo/phone.svg'
 import korzina from '../../img/logo/korzina.svg'
 import arrow from '../../img/logo/arrow.svg'
 import logo from '../../img/logo/LOgoFood-planet.svg'
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
+
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <img src={logo} alt="" />
             <ul>
-                <li><a href="#">Главная</a></li>
-                <li><a href="#">Меню<img src={arrow} alt="" /></a></li>
-                <li><a href="#">Доставка</a></li>
-                <li><a href="#">Контакты</a></li>
+                <li><Link to="/">Главная</Link></li>
+                <li><Link to="/menu" >Меню<img src={arrow} alt="" /></Link></li>
+                <li><Link to='/delivery'>Доставка</Link></li>
+                <li><Link to="/contacts">Контакты</Link></li>
                 <li><a href="#"><img src={phone} alt="" />+996500405988</a></li>
             </ul>
         </footer>
